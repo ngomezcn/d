e2443ff82c35ea6468105f723379d491caa7d1c9
Naïm Gómez <naimgomezcn@gmail.com>
2023-03-19 12:46:35 +0100

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -14,4 +14,23 @@ namespace SAP {
     export function test(sound?: AlarmSound) : number {
        return 1
     }
+
+    /**
+         * Address led
+         */
+    //% blockId=foo_B
+    //% block="foo with $callback"
+    export function foo(callback: Function) {
+        console.log('foo() function called!');
+        callback();
+    }
+
+    /**
+             * Address led
+             */
+    //% blockId=bar_B
+    //% block="bar"
+    export function bar() {
+        console.log('bar() function called!');
+    }
 }
