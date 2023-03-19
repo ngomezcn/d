272efd6ecacd3a6fa5826e5cd9cd192db4ae5ec9
Naïm Gómez <naimgomezcn@gmail.com>
2023-03-19 13:16:11 +0100

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -5,35 +5,10 @@ enum AlarmSound {
     CardName
 }
 namespace SAP {
-    /**
-     * Address led
-     */
     //% blockId=BusinessPartner_B
     //% block="BusinessPartner $sound"
     //% expandableArgumentMode="toggle"
-    export function test(sound?: AlarmSound): number {
-        return 1
-    }
-
-    /**
-         * Address led
-         */
-    //% blockId=foo_B
-    //% block="foo with $callback"
-    export function foo(callback: Function) {
-        console.log('foo() function called!');
-        callback();
-    }
-}
-
-namespace Salesforce {
-    /**
-     * Address led
-     */
-    //% blockId=BusinessPartner_B
-    //% block="BusinessPartner $sound"
-    //% expandableArgumentMode="toggle"
-    export function test(sound?: AlarmSound): number {
+    export function BusinessPartner(sound?: AlarmSound): number {
         return 1
     }
 
