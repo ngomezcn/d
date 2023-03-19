de8b48bd1c30f2019aa6205fe74f6306e7fe655c
Naïm Gómez <naimgomezcn@gmail.com>
2023-03-19 12:35:24 +0100

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 namespace SAP {
     /**
-     * 
+     * Address led
      */
     //% blockId=testB
-    export function test(x: number, y: number) : number {
-        return x+y
+    //% block="plot at $x"
+    export function test(x: number) : number {
+        return x+1
     }
 }
