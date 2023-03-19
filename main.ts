270b813dc935cb74f1b14a3707c11502a4b1f1fb
Naïm Gómez <naimgomezcn@gmail.com>
2023-03-19 13:19:23 +0100

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
-enum AlarmSound {
+enum BusinessPartnerE {
     //% block="CardCode"
     CardCode,
     //% block="CardName"
     CardName
 }
+
+enum OrderE {
+    //% block="DocEntry"
+    DocEntry,
+    //% block="DocNum"
+    DocNum
+}
 namespace SAP {
     //% blockId=BusinessPartner_B
     //% block="BusinessPartner $sound"
-    export function BusinessPartner(sound?: AlarmSound): number {
+    export function BusinessPartner(sound?: BusinessPartnerE): any {
+        return 1
+    }
+
+    //% blockId=Order_B
+    //% block="Order $sound"
+    export function Order(sound?: OrderE): any {
         return 1
     }
 
@@ -16,8 +29,7 @@ namespace SAP {
          */
     //% blockId=foo_B
     //% block="foo with $callback"
-    export function foo(callback: Function) {
+    export function foo(callback: any) {
         console.log('foo() function called!');
-        callback();
     }
 }
